295ab75edadb161980514036dd50f319380c35fd
Windu Purnomo <wpwindupurnomo@gmail.com>
2015-01-27 18:53:06 +0700

    template architecture

---
+++ js/local/app.js
@@ -0,0 +1,45 @@
+angular.module("pelangi", ["ui.router"])
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '../../templates/home.html'
+        })
+        .state('list', {
+            url: '/list',
+            templateUrl: 'templates/list.html',
+            controller: 'ListCtrl'
+        })
+        .state('list.item', {
+            url: '/:item',
+            templateUrl: 'templates/list.item.html',
+            controller: function($scope, $stateParams) {
+                $scope.item = $stateParams.item;
+            }
+        })
+    $urlRouterProvider.otherwise('/home');
+})
+
+.controller("ListCtrl", function($scope) {
+    $scope.shoppingList = [{
+        name: 'Milk'
+    }, {
+        name: 'Eggs'
+    }, {
+        name: 'Bread'
+    }, {
+        name: 'Cheese'
+    }, {
+        name: 'Ham'
+    }];
+
+    $scope.selectItem = function(selectedItem) {
+        _($scope.shoppingList).each(function(item) {
+            item.selected = false;
+            if (selectedItem === item) {
+                selectedItem.selected = true;
+            }
+        });
+    };
+})
